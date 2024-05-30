faa1e5bac65b539f852e9f6da6df58923306a89a
akashp73387 <akashp73387@gmail.com>
2024-05-30 15:54:36 +0530

    updated

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,22 +1,16 @@
  // routes/routes.js
 
 import express from 'express';
-import { createBooking, viewAllBookings } from '../Controllers/BookingController.js';
-import { viewAllRooms, createRoom } from '../Controllers/RoomController.js';
-import { listAllCustomers, viewCustomerBookings } from '../Controllers/CustomerController.js';
+import { bookRoom, createRoom, getCustomerName, getCustomers, getRooms, viewRooms } from '../Controllers/RoomController.js';
 
 const router = express.Router();
 
-// Booking Routes
-router.post('/booking/create/:id', createBooking);
-router.get('/booking/viewbooking', viewAllBookings);
+router.get('/rooms',getRooms);   //get all rooms
+router.post('/create',createRoom); // create new room
+router.post('/booking/create/:id',bookRoom); // book a room
+router.get('/loadbookings',viewRooms);  // get all bookings 
+router.get('/customers',getCustomers); //get all customers
+router.get('/customer/:name',getCustomerName); // list details of a customer with name
 
-// Room Routes
-router.get('/rooms/all', viewAllRooms);
-router.post('/rooms/create', createRoom);
-
-// Customer Routes
-router.get('/customers', listAllCustomers);
-router.get('/customers/:name', viewCustomerBookings);
 
 export default router;
